dee77f942e1f37996f53f6e273649da4a653070c
selinustaoglu <76212021+selinustaoglu@users.noreply.github.com>
2022-06-16 12:03:41 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+var counterContainer = document.querySelector(".website-counter");
+var visitCount = localStorage.getItem("page_view");
+
+if(visitCount){
+    localStorage.setItem("page_view", ++visitCount);
+} else {
+    visitCount = 1;
+    localStorage.setItem("page_view",1);
+}
+
+counterContainer.innerHTML = visitCount;
+
+
+
+
+
